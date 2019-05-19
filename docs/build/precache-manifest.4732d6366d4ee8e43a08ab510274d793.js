d4c0812a98a1fc4a22b41310c28ea73901a58e05
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-05-19 02:38:41 +0200

    Update docs

---
+++ docs/build/precache-manifest.4732d6366d4ee8e43a08ab510274d793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fbfd31d4dbf177a6c2b4f590bc6291",
+    "url": "./index.html"
+  },
+  {
+    "revision": "952eac0d4a0e0bab92ef",
+    "url": "./static/css/main.ba276a45.chunk.css"
+  },
+  {
+    "revision": "eb4ee1642a5aec871755",
+    "url": "./static/js/2.eae0bcdb.chunk.js"
+  },
+  {
+    "revision": "952eac0d4a0e0bab92ef",
+    "url": "./static/js/main.511b7018.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
